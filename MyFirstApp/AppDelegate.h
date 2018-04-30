07f03be2f9ba7bfdd57ccd2e85f14ef0c941df44
Juhnowski <juhnowski@gmail.com>
2018-04-30 23:49:35 +0300

    Initial Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by SBT on 30/04/2018.
+//  Copyright © 2018 SBT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by SBT on 30/04/2018.
+//  Copyright © 2018 SBT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
